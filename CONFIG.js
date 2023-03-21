88ed97a06d348b737a470eabaed0ddf473090def
nnnttthhh01082004 <128230317+nnnttthhh01082004@users.noreply.github.com>
2023-03-21 23:45:24 +0700

    lan2
    222

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+const CONFIGDATA = {
+  titleHeader: "Hello sister Quyên, Let's be friends",
+  descriptionHeader:
+    "Nếu chị thoát ra và không trả lời thì có nghĩa là chị đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn chị vì đã đồng ý",
+  descriptionModar:
+    'Em biết là chị sẽ đồng ý làm bạn với em mà ^^ </br> Cám ơn chị đã đồng ý để cho emđược làm bạn với chị. Đối với em, tình bạn là tuyệt vời hơn cả :)),<br> \t\t\t\t\tThank sister for being friends with me!! ^^',
+  
+  
+};
